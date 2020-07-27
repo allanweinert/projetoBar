4ceb7ad688ea61a9c8770ee89aabbf86d8e056f3
Allan Weinert <allan_weinert@hotmail.com>
2020-07-27 19:52:34 -0300

    Modelo de gráfico interno

--- front-web/src/app/pagina-inicial/dashboard/graficos/grafico.component.ts
+++ front-web/src/app/pagina-inicial/dashboard/graficos/grafico.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as Highcharts from 'highcharts';
 import { Router } from '@angular/router';
+
 import { DashboardPesquisaService } from '../services/dashboard-pesquisa.service';
 
 @Component({
@@ -9,8 +10,11 @@ import { DashboardPesquisaService } from '../services/dashboard-pesquisa.service
   styleUrls: ['./grafico.component.css']
 })
 export class GraficoComponent implements OnInit {
-  title: string = "Grafico Principal";
+
+  title: string = "Bem - Vindo!";
+
   users: any = [];
+  saldos: any = [];
 
   Highcharts: typeof Highcharts = Highcharts;
   grafico: Highcharts.Options = {
@@ -29,6 +33,14 @@ export class GraficoComponent implements OnInit {
         lang: {
         noData: 'https://www.highcharts.com/samples/graphics/sun.png'
     },
+    title: {
+      text: 'Gráfico de teste'
+    },
+    plotOptions: {
+      column: {
+          borderRadius: 5
+      }
+    },
     noData: {
         style: {
             fontWeight: 'bold',
@@ -38,26 +50,85 @@ export class GraficoComponent implements OnInit {
     }
   };
 
-  constructor(public userService: DashboardPesquisaService,
-    public router: Router) { }
+
+  /*Highcharts: typeof Highcharts = Highcharts;
+  grafico: Highcharts.Options = {
+    chart: {
+      type: 'column'
+    },
+    xAxis: {
+      categories: []
+    },
+    series: [
+      {
+        data: [],
+        type: 'column'
+      }
+    ],
+        lang: {
+        noData: 'https://www.highcharts.com/samples/graphics/sun.png'
+    },
+    title: {
+      text: 'Saldo por produto'
+    },
+    plotOptions: {
+      column: {
+          borderRadius: 5
+      }
+    },
+    noData: {
+        style: {
+            fontWeight: 'bold',
+            fontSize: '15px',
+            color: '#303030'
+        }
+    }
+  };*/
+
+  constructor(
+    public userService: DashboardPesquisaService,
+    public produtoService: DashboardPesquisaService,
+    public router: Router
+    ) { }
 
     ngOnInit() {
-        this.userService.getUsers()
-          .subscribe(
-            (data) => { // Success
-                this.users = data['results'];
-                const dadosGrafico = this.users.map(x => x.dob.age);
-                const nome = this.users.map(x  => x.name.first);
+      //inicio grafico de teste
+      this.userService.getUsers()
+        .subscribe(
+          (data) => { // Success
+            this.users = data['results'];
+            const dadosGrafico = this.users.map(x => x.dob.age);
+            const nome = this.users.map(x  => x.name.first);
     
-                //Highcharts
-                this.grafico.series[0]['data'] = dadosGrafico;
-                this.grafico.xAxis['categories'] = nome;
-                Highcharts.chart('MediosdPPrincipal', this.grafico);
-            },
-            (err) => {
+            //Highcharts
+            this.grafico.series[0]['data'] = dadosGrafico;
+            this.grafico.xAxis['categories'] = nome;
+            Highcharts.chart('MediosdPPrincipal', this.grafico);
+          },
+          (err) => {
                 console.error(err);
-            }
-          );
-      }
+          }
+        );
+      //fim do gráfico de teste
+
+      //inicio do gráfico teste interno
+      /*this.produtoService.saldoEstoque()
+        .subscribe(
+          (data) => { // Success
+            this.saldos = data['results'];
+            const dadosGrafico = this.saldos.map(x => x.restante);
+            const graficoProduto = this.saldos.map(x  => x.produto);
+            
+            //Highcharts
+            this.grafico.series[0]['data'] = dadosGrafico;
+            this.grafico.xAxis['categories'] = graficoProduto;
+            Highcharts.chart('MediosdPPrincipal', this.grafico);
+          },
+          (err) => {
+                console.error(err);
+          }
+        );*/
+      //fim do grafico teste interno
+    }
 
 }
