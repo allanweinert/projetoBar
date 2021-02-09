09bf004e82e2a359ca6ed0cf12f504ca3877110a
Allan Weinert <allan_weinert@hotmail.com>
2021-02-08 21:10:21 -0300

    Removido JSON Ignore. Removido Map. ItemEntrada

--- server-api/src/main/java/br/inf/ids/treina1/api/movimentacao/itens/ItemEntrada.java
+++ server-api/src/main/java/br/inf/ids/treina1/api/movimentacao/itens/ItemEntrada.java
@@ -1,9 +1,7 @@
 package br.inf.ids.treina1.api.movimentacao.itens;
 
 import java.math.BigDecimal;
-import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,26 +9,23 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
-import javax.persistence.Transient;
-import javax.validation.Valid;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import br.inf.ids.treina1.api.movimentacao.MovimentacaoEstoque;
-import br.inf.ids.treina1.api.movimentacao.saldoestoque.SaldoEstoque;
 import br.inf.ids.treina1.api.produto.Produto;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "itementrada")
 @SequenceGenerator(name = "seq_itementrada", sequenceName = "seq_itementrada", allocationSize = 1)
-
+@Getter @Setter
 public class ItemEntrada {
 
 	@Id
@@ -45,7 +40,6 @@ public class ItemEntrada {
 	
 	@ManyToOne
 	@JoinColumn(name = "produtoid")
-	@JsonIgnoreProperties({"categoria", "estoque_minimo", "situacao"})
 	private Produto produto;
 	
 	@NotNull
@@ -60,79 +54,8 @@ public class ItemEntrada {
 	@NotNull
 	@Column(precision = 5, scale = 2)
 	private BigDecimal total;
+
+	@NotNull
+	private Integer restante;
 	
-	@ManyToOne
-	@Transient //Utilizado para não gravar em banco.
-    @JoinColumn(name = "ITEMENTRADAID")
-    private SaldoEstoque saldo;
-	
-	@Valid
-	@OneToMany(mappedBy = "itemEntrada", cascade = CascadeType.ALL, orphanRemoval = true)
-	private List<SaldoEstoque> saldoEstoque;
-	
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public MovimentacaoEstoque getMovimentacaoEstoque() {
-		return movimentacaoEstoque;
-	}
-
-	public void setMovimentacaoEstoque(MovimentacaoEstoque movimentacaoEstoque) {
-		this.movimentacaoEstoque = movimentacaoEstoque;
-	}
-
-	public Produto getProduto() {
-		return produto;
-	}
-
-	public void setProduto(Produto produto) {
-		this.produto = produto;
-	}
-
-	public Integer getQuantidade() {
-		return quantidade;
-	}
-
-	public void setQuantidade(Integer quantidade) {
-		this.quantidade = quantidade;
-	}
-
-	public BigDecimal getValorUnitario() {
-		return valorUnitario;
-	}
-
-	public void setValorUnitario(BigDecimal valorUnitario) {
-		this.valorUnitario = valorUnitario;
-	}
-	
-	public BigDecimal getTotal() {
-		return valorUnitario;
-	}
-
-	public SaldoEstoque getSaldo() {
-		return saldo;
-	}
-
-	public void setSaldo(SaldoEstoque saldo) {
-		this.saldo = saldo;
-	}
-
-	public List<SaldoEstoque> getSaldoEstoque() {
-		return saldoEstoque;
-	}
-
-	public void setSaldoEstoque(List<SaldoEstoque> saldoEstoque) {
-		this.saldoEstoque = saldoEstoque;
-	}
-
-	public void setTotal(BigDecimal total) {
-		this.total = total;
-	}
-
 }
