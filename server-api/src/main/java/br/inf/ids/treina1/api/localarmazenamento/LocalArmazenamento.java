2b4849ec0b17f233024ba09597ec2899c403f900
Allan Weinert <allan_weinert@hotmail.com>
2021-02-08 21:08:12 -0300

    Incluído local de armazenamento Id

--- server-api/src/main/java/br/inf/ids/treina1/api/localarmazenamento/LocalArmazenamento.java
+++ server-api/src/main/java/br/inf/ids/treina1/api/localarmazenamento/LocalArmazenamento.java
@@ -1,19 +1,26 @@
 package br.inf.ids.treina1.api.localarmazenamento;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import br.inf.ids.treina1.api.localarmazenamento.enums.Situacao;
 import lombok.Getter;
 import lombok.Setter;
-
-import javax.persistence.*;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 @Entity
 @SequenceGenerator(name = "SEQ_LOCALARMAZENAMENTO", sequenceName = "SEQ_LOCALARMAZENAMENTO", allocationSize = 1)
 @Table(name = "localarmazenamento")
 @Getter
 @Setter
-
 public class LocalArmazenamento {
 
     @Id
@@ -29,4 +36,10 @@ public class LocalArmazenamento {
     @Enumerated(EnumType.STRING)
     private Situacao situacao;
        
+    public LocalArmazenamento(){    	
+    }
+    
+    public LocalArmazenamento(Long id) {
+    	this.id = id;
+    }
 }
