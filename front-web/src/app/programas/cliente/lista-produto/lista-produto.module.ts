d25cff94d5cf4f8c0063864bb556550259e98717
Allan Weinert <allan_weinert@hotmail.com>
2021-02-08 22:26:34 -0300

    Criação da lista de produtos.

---
+++ front-web/src/app/programas/cliente/lista-produto/lista-produto.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListaProdutoComponent } from './lista-produto.component';
+
+
+
+@NgModule({
+  declarations: [ListaProdutoComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ListaProdutoModule { }
